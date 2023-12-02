1e735d6815dc688fa5080dbd11720effff145a8a
unknown <quysmart123@gmail.com>
2023-12-02 18:41:25 +0700

    first test

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
+describe('first test', () => {
+  it('error toast appear when wrong credentials', () => {
+    cy.visit('http://localhost:3000/login')
+    cy.get('input[name=employeeNo]').type('superadmin')
+    cy.get('input[name=password]').type('abc')
+    cy.contains('Log in').click();
+    cy.contains('Incorrect username or password').should('exist')
   })
 })
