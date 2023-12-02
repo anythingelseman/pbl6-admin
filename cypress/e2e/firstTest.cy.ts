c720e969cce75823032c0b779c25e526413ec8a5
unknown <quysmart123@gmail.com>
2023-12-02 22:00:33 +0700

    fix category api endpoint

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -6,4 +6,12 @@ describe('first test', () => {
     cy.contains('Log in').click();
     cy.contains('Incorrect username or password').should('exist')
   })
+
+  it('login succesfully', () => {
+    cy.visit('http://localhost:3000/login')
+    cy.get('input[name=employeeNo]').type('superadmin')
+    cy.get('input[name=password]').type('Abc123!@#')
+    cy.contains('Log in').click();
+    cy.contains('Log in successfully').should('exist')
+  })
 })
